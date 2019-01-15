6a168d47433a3df7e3a21b768641d90468afd641
MyNameIsTaeYeong <imtang1993@naver.com>
2019-01-15 17:35:25 +0900

    다음 순열

--- BOJ_10973.cpp
+++ BOJ_10973.cpp
@@ -1,50 +1,9 @@
-#include<cstdio>
-using namespace std;
+//
+//  BOJ_10973.cpp
+//  Algorithm_Mac
+//
+//  Created by TaeYeong on 15/01/2019.
+//  Copyright © 2019 TaeYeong. All rights reserved.
+//
 
-int permu[10000];
-
-void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-bool prev_permutation(int *a, int n){
-    //1. a[i] > a[i+1]중 가장 작은 i
-    int i = 0;
-    while(i<n-1 && a[i]<=a[i+1]) i++;
-    if(i>=n-1) return false; // 마지막 순열
-    
-    //2. j<=i중 a[j] > a[i+1]를 만족시키는 가장 작은 j
-    int j = 0;
-    while(a[j] <= a[i+1]) j++;
-    
-    //3. a[i-1] 와 a[j] swap
-    swap(&a[i+1], &a[j]);
-    
-    //4. a[i]부터 뒤집기
-    j = n-1;
-    while(i<j){
-        swap(&a[i+1], &a[j]);
-        i++; j--;
-    }
-    
-    return true;
-}
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    for(int i=0; i<n; i++)
-        scanf("%d", &permu[i]);
-    
-    if(prev_permutation(permu, n)){
-        for(int i=0; i<n; i++)
-            printf("%d ", permu[i]);
-    }else{
-        printf("%d", -1);
-    }
-    puts("");
-    return 0;
-}
+#include "BOJ_10973.hpp"

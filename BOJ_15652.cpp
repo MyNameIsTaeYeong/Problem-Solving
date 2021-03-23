2736f0d9b8fdde26d7b5a3b35b33b7d8747fe5d1
TaeYeong Im <imtaebari@gmail.com>
2021-03-23 16:09:50 +0900

    N과 M (4)

---
+++ BOJ_15652.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int ans[10];
+
+void go(int index, int start, int m, int n){
+    if(index == m){
+        for(int i=0; i<m; i++){
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    
+    if(start == n){
+        return;
+    }
+    
+    for(int i=start; i<n; i++){
+        ans[index] = i+1;
+        go(index+1, i, m, n);
+    }
+    
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    go(0, 0, m, n);
+    
+    return 0;
+}

c51861e051818f674562cc0248b8926eba100d46
TaeYeong Im <imtaebari@gmail.com>
2021-04-06 17:27:49 +0900

    위장

---
+++ programmers_#3.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> m;
+    
+   for(int i=0; i<clothes.size(); i++){
+        auto it = m.find(clothes[i][1]);
+        if(it == m.end()){
+            m[clothes[i][1]] = 1;
+        }else{
+            m[clothes[i][1]] += 1;
+        }
+    }
+    
+    
+    for(auto it = m.begin(); it != m.end(); it++){
+        (it->second)++;
+        answer *= (it->second);
+    }
+    answer--;
+    return answer;
+}

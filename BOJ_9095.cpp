794ff842ff290841546eb788ab3c9b54225b0836
TaeYeong Im <imtaebari@gmail.com>
2021-04-27 16:31:03 +0900

    1, 2, 3 더하기

--- BOJ_9095.cpp
+++ BOJ_9095.cpp
@@ -2,39 +2,25 @@
 
 using namespace std;
 
-int solve(int n, int sum)
-{
-    if(sum > n){
-        return 0;
-    }
-    
-    if(sum == n){
-        return 1;
-    }
-    
-    int ans = 0;
-    
-    for(int i=1; i<=3; i++){
-        ans += solve(n, sum+i);
-    }
-    
-    return ans;
-}
+int memo[11];
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    memo[1] = 1;
+    memo[2] = 2;
+    memo[3] = 4;
+    
+    for(int i=4; i<11; i++){
+        memo[i] = memo[i-1] + memo[i-2] + memo[i-3];
+    }
     
     int t;
     cin >> t;
     
-    while(t--){
+    while (t--) {
         int n;
         cin >> n;
-        
-        cout << solve(n, 0) << '\n';
+        cout << memo[n] << '\n';
     }
     
     return 0;
